8deae8ea48f191827db513c60f3dea4af64a2f67
John Grib <johngrib@woowahan.com>
2017-04-04 23:57:59 +0900

    주석 추가

--- src/CalcFactory.java
+++ src/CalcFactory.java
@@ -5,10 +5,13 @@ import java.util.regex.Pattern;
 public class CalcFactory {
 
 
+    /**
+     * 주어진 문자열이 어느 CalcRule 객체에 적합한지 판별하여, 해당 CalcRule 객체를 리턴한다.
+     */
     static CalcRule create(final String input) {
 
         if(input == null) {
-            return  null;
+            return  new NoneCalc(input);
         }
 
         final CalcRule[] rules = new CalcRule[] {
@@ -17,7 +20,7 @@ public class CalcFactory {
                 new NoneCalc(input)
         };
 
-        CalcRule rule = Arrays.stream(rules)
+        final CalcRule rule = Arrays.stream(rules)
                 .filter(r -> r.isMatchedType(input))
                 .findFirst()
                 .get()
@@ -27,11 +30,24 @@ public class CalcFactory {
     }
 }
 
+/**
+ * 계산 규칙 객체 인터페이스
+ */
 interface CalcRule {
+    /**
+     * 주어진 문자열이 계산 규칙 객체가 지원하는 타입인지 검사한다.
+     */
     boolean isMatchedType(final String input);
+
+    /**
+     * 값을 계산해 리턴한다.
+     */
     int calc();
 }
 
+/**
+ * 계산 규칙 공통
+ */
 abstract class CalcCommon implements CalcRule {
 
     final String inputSentence;
@@ -81,6 +97,9 @@ abstract class CalcCommon implements CalcRule {
     }
 }
 
+/**
+ * 기본 규칙은 , 와 : 를 delimeter 로 삼는다.
+ */
 class BasicCalc extends  CalcCommon {
 
     public BasicCalc(String inputSentence) {
@@ -88,20 +107,34 @@ class BasicCalc extends  CalcCommon {
         super.regex = "^\\d+(?:[,\\:]\\d+)*$|^$";
         super.delimeter = "\\:|\\,";
     }
+
+    /**
+     * 음수는 취급하지 않는다.
+     */
+    boolean validate(int number) {
+        return number >= 0;
+    }
 }
 
+/**
+ * 잘못된 입력을 처리하는 계산 규칙
+ */
 class NoneCalc extends CalcCommon {
 
     public NoneCalc(String inputSentence) {
         super(inputSentence);
         super.regex = "^.*$";
     }
+
     @Override
     public int calc() {
-       throw new RuntimeException("알 수 없는 규칙입니다.");
+       throw new RuntimeException("정의되지 않은 규칙입니다.");
     }
 }
 
+/**
+ * 커스텀 구분자를 처리하는 계산 규칙
+ */
 class CustomCalc extends CalcCommon {
 
     private String source = null;
@@ -124,6 +157,9 @@ class CustomCalc extends CalcCommon {
         return super.calc();
     }
 
+    /**
+     * 커스텀 구분자는 regex quoting 처리
+     */
     @Override
     String getDelimeter() {
         return Pattern.quote(this.delimeter);
@@ -134,6 +170,9 @@ class CustomCalc extends CalcCommon {
         return this.source;
     }
 
+    /**
+     * 음수는 취급하지 않는다.
+     */
     @Override
     boolean validate(int number) {
         return number >= 0;
